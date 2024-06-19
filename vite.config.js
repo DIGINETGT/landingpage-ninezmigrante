b53d72d38b20cdbd78708ce96a7b067d5140bd87
alexsantos-eth <alexdsantosv@gmail.com>
2024-06-19 00:06:08 -0600

    [ Fix ] vite out dir

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
+    outDir: path.join(__dirname, "dist"),
     rollupOptions: {
       onwarn(warning, warn) {
         if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
